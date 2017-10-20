fc5d649ba32e5517fa41044d50c2e8796e723152
Daniel Ku <danieljku@gmail.com>
2017-10-20 15:25:30 -0700

    Initial commit

---
+++ GooglePlacesExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GooglePlacesExercise
+//
+//  Created by Daniel Ku on 10/20/17.
+//  Copyright © 2017 djku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
